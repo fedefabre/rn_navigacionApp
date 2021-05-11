4a0f926223d163f29f648bf99e3809df81b59bab
Federico-Fabre <federico.fabre@xpo.com>
2021-05-11 08:47:29 -0300

    Adding icons to android

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { View, Text } from 'react-native'
+import { styles } from '../theme/appTheme'
 
 const Tab1Screen = () => {
 
   useEffect(() => {
-      console.log('Tab1Screen effect')
+    console.log('Tab1Screen effect')
   }, [])
 
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>
+      <Text>
+        <Icon name="ios-person" size={30} color="#4F8EF7" />;
+      </Text>
     </View>
   )
 }
