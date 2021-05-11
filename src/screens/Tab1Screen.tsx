2ac52849fd83a6b45214305abd0c068dc24e33e1
Federico-Fabre <federico.fabre@xpo.com>
2021-05-11 09:43:03 -0300

    Adding icons to first screen

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { View, Text } from 'react-native'
-import { styles } from '../theme/appTheme'
+import { colores, styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Tab1Screen = () => {
+
+  const { top: marginTop } = useSafeAreaInsets();
 
   useEffect(() => {
     console.log('Tab1Screen effect')
   }, [])
 
   return (
-    <View style={styles.globalMargin}>
+    <View style={{ ...styles.globalMargin, marginTop }}>
       <Text style={styles.title}>Iconos</Text>
       <Text>
-        <Icon name="ios-person" size={30} color="#4F8EF7" />;
+        <Icon name="ios-person" size={80} color={colores.primary} />
+        <Icon name="ear-outline" size={80} color={colores.primary} />
+        <Icon name="partly-sunny-outline" size={80} color={colores.primary} />
+        <Icon name="earth-outline" size={80} color={colores.primary} />
       </Text>
     </View>
   )
